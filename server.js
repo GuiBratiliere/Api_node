97d1f3185759eeba90a725a3b17c79dce4a6912f
Guilherme Bratiliere <110342103+GuiBratiliere@users.noreply.github.com>
2024-12-11 15:25:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-import express from "express";
-import routes from "./src/routes/postRoutes.js";
+import express from "express"; // Importa o framework Express para construir o servidor
+import routes from "./src/routes/postRoutes.js"; // Importa as rotas definidas no arquivo "postRoutes.js"
 
-const app = express();
-app.use(express.static('uploads'))
-routes(app)
+const app = express(); // Cria uma instância do servidor Express
 
+// Configura o middleware para servir arquivos estáticos na pasta 'uploads'
+// Isso permite acessar arquivos armazenados na pasta 'uploads' diretamente pela URL
+app.use(express.static('uploads'));
+
+// Chama a função de rotas e passa a instância do servidor como argumento
+// Define as rotas da aplicação com base nas funções exportadas no arquivo "postRoutes.js"
+routes(app);
+
+// Configura o servidor para escutar na porta 3000
+// Quando o servidor está em execução, exibe uma mensagem de confirmação no console
 app.listen(3000, () => {
-    console.log('Servidor escutando...');
+    console.log('Servidor escutando...'); // Mensagem de log indicando que o servidor está rodando
 });
-
-
